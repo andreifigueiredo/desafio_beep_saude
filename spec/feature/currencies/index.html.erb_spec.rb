55341c8d543fe2ec064773513dc92a9c36897c93
Andrei <projetocionline2@culturainglesa.net>
2019-06-23 00:21:21 -0300

    fix_text_and_tests

--- spec/feature/currencies/index.html.erb_spec.rb
+++ spec/feature/currencies/index.html.erb_spec.rb
@@ -4,8 +4,16 @@ RSpec.describe "currencies/index.html.erb", type: :feature do
   it "click in all tabs" do
     visit '/currencies/index'
     find('#tab2').click
+    expect(page).to have_selector("//.tab2", visible: true)
+    expect(page).to have_selector("//.tab3", visible: false)
+    expect(page).to have_selector("//.tab1", visible: false)
     find('#tab1').click
+    expect(page).to have_selector("//.tab1", visible: true)
+    expect(page).to have_selector("//.tab2", visible: false)
+    expect(page).to have_selector("//.tab3", visible: false)
     find('#tab3').click
-    expect(page).to have_content 'PESO ARGENTINO'
+    expect(page).to have_selector("//.tab3", visible: true)
+    expect(page).to have_selector("//.tab2", visible: false)
+    expect(page).to have_selector("//.tab1", visible: false)
   end
 end
